feb28119ef3218587a8c6536c49c325f64ec8b09
Maureen-1998DEV <maureen.ougo@student.moringaschool.com>
2021-07-31 14:02:14 +0300

    modified the js file

--- js/script.js
+++ js/script.js
@@ -7,26 +7,41 @@ var CustomerOder = function (pizzaSize, toppingsSelection, crustOption, numberOf
     this.deliveryOption = deliveryOption;
     this.inputLocation = inputLocation;
 }
+//pizza size,toppings,crust prize//
 CustomerOder.prototype.NalaDeliousPizza = {
     name = "Nala Delious Pizza",
 
     pizzaSizePrice=[
-        { selectPizzaSize=0 },
+        { defaultStatus=0},
         { small=600 },
         { medium=800 },
         { large = 1000 },
         { extraLarge = 1200 }],
 
     toppingsSelectionPrize =[
-        { Toppings = 0 },
+        { defaultStatus = 0 },
         { Pepperoni = 100 },
         { GarlicButterPrawnsandChilli = 300 },
         { Supreme = 200 }],
 
     crustOptionPrize =[
-        { crust=0 },
+        { defaultStatus=0 },
         { crispy = 100 },
         { stuffed = 150 },
         { glutten_free = 200 }],
 
 }
+$(document).ready(function(){
+    $("#orderButton").click(function(event){
+        event.preventDefault()
+
+
+        var pizzaSize=$("#pizzaSize").val();
+        var toppingsSelection=$("#toppingsSelection").val();
+        var crustOption=$("#crustOption").val();
+        var numberOfPizza=$("#numberOfPizza").val();
+        var deliveryOption=$("#deliveryOption").val();
+        var inputLocation=$("#inputLocation").val();
+    var CustomerOder = new CustomerOrder()
+    })
+})
